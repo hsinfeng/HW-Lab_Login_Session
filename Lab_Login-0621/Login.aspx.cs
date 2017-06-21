6214beac57662dab32796260e589a20b13f6f96a
hsinfeng <judy4a031004@gmail.com>
2017-06-21 17:30:36 +0800

    Add files via upload

---
+++ Lab_Login-0621/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (txtUserName.Text =="") {
+           lblPrompt.Text=" Pls key in your UserName.";
+            return;
+        }
+        if (txtPassword.Text == "") {
+            lblPrompt.Text = " Pls key in your Password.";
+            return;
+        }
+
+
+
+        Session["userName"] = txtUserName.Text;
+        if (Session["userName"] == null) {
+            Response.Redirect("Login.aspx");
+        } 
+        else {
+            Response.Redirect("Secret.aspx");
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e) {
+        txtUserName.Text = "";
+        txtPassword.Text = "";
+
+    }
+}
+
+
+
+ 
---
+++ Lab_Login-0621/Secret.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Secret.aspx.cs" Inherits="Secret" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p>
+    Content for member only.</p>
+<p>
+    <asp:Button ID="LogOutButton" runat="server" OnClick="LogOutButton_Click" Text="Logout" Width="98px" />
+    <br />
+</p>
+<p>
+</p>
+</asp:Content>
+
